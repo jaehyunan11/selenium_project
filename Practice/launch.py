b3ab5a9210564423916600334f0daf68692ade28
jaehyun An <jaehyuna11@gmail.com>
2021-02-26 09:30:17 -0800

    first commit

---
+++ Practice/launch.py
@@ -0,0 +1,10 @@
+import time
+from selenium import webdriver
+driver = webdriver.Chrome('/Users/jaehyunan/Desktop/Selenium/chromedriver')
+driver.get('http://www.google.com/')
+time.sleep(5)
+search_box = driver.find_element_by_name('q')
+search_box.send_keys('ChromeDriver')
+search_box.submit()
+time.sleep(5)
+driver.quit()
---
+++ Udemy/classes.py
@@ -0,0 +1,10 @@
+from math import sqrt
+
+
+class ModulesDemo():
+    def builtin_modules(self):
+        print(sqrt(100))
+
+
+m = ModulesDemo()
+m.builtin_modules()
