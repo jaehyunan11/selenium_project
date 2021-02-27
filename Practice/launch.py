2428830136debb8c68e04de879ef8a3a6e9e401d
jaehyun An <jaehyuna11@gmail.com>
2021-02-26 20:39:06 -0800

    update launch.py

--- Practice/launch.py
+++ Practice/launch.py
@@ -1,10 +1,22 @@
-import time
 from selenium import webdriver
-driver = webdriver.Chrome('/Users/jaehyunan/Desktop/Selenium/chromedriver')
-driver.get('http://www.google.com/')
-time.sleep(5)
-search_box = driver.find_element_by_name('q')
-search_box.send_keys('ChromeDriver')
-search_box.submit()
-time.sleep(5)
-driver.quit()
+import time
+
+
+class ChromeDriverMac():
+
+    def test(self):
+        # instantiate Chrome Browser Command
+        driver = webdriver.Chrome(
+            '/Users/jaehyunan/Desktop/Selenium/chromedriver')
+        # Open the privded URL
+        driver.get('http://www.google.com/')
+        time.sleep(5)
+        search_box = driver.find_element_by_name('q')
+        search_box.send_keys('ChromeDriver')
+        search_box.submit()
+        time.sleep(5)
+        driver.quit()
+
+
+cc = ChromeDriverMac()
+cc.test()
